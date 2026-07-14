e4102bcf5f4aaf73ccd22bf9616f7365a6167e88
RishitaDugar4 <rishitdugar@gmail.com>
2026-07-14 13:22:09 -0700

    complete orderbook with different order types

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Orderbook.h"
+
+int main() {
+
+    Orderbook orderbook;
+    // const OrderId orderid = 1;
+    // orderbook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, orderid, Side::Buy, 100, 10));
+    // std::cout << orderbook.Size() << std::endl; // 1
+    // orderbook.CancelOrder(orderid);
+    // std::cout << orderbook.Size() << std::endl; //0
+
+    return 0;
+}
